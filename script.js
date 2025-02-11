7dcede093da29fd91515a40f785b16f22889692f
Luka Popovic <lukapopovic0111@gmail.com>
2025-02-11 22:25:25 +0100

    year

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const ime = "Lazar";
 let prezime = "Vuckovic";
 prezime = "Petrovic";
 console.log(`Moje ime je ${ime} al prezime ${prezime}.`);
+let godine = 20;
+if (godine < 20) {
+  console.log(`Godine su manje od 20`);
+} else {
+  console.log(`Godine su vece od 20`);
+}
