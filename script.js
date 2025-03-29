d7abf14e7a40d9468587ec9d1f8990650054842f
Luka Popovic <lukapopovic0111@gmail.com>
2025-03-29 11:05:48 +0100

    Making Functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log(`Moj prvi projekat.`);
+/*console.log(`Moj prvi projekat.`);
 const ime = "Lazar";
 let prezime = "Vuckovic";
 prezime = "Petrovic";
@@ -8,8 +8,30 @@ if (godine < 20) {
   console.log(`Godine su manje od 20`);
 } else {
   console.log(`Godine su vece od 20`);
-}
+}*/
 //Counter
-const minus = document.querySelector("#btn-1");
-const reset = document.querySelector("#btn-2");
-const plus = document.querySelector("#btn-3");
+const btnOne = document.querySelector("#btn-1");
+const btnTwo = document.querySelector("#btn-2");
+const btnThree = document.querySelector("#btn-3");
+const number = document.querySelector("p");
+
+function calculate(numberOne) {
+  if (numberOne === btnOne) {
+    number.textContent = Number(number.textContent) - 1;
+  } else {
+    number.textContent = Number(number.textContent) + 1;
+  }
+}
+function color() {
+  if (number.textContent < 0) {
+    number.style.color = "red";
+  } else if (number.textContent > 0) {
+    number.style.color = "green";
+  } else {
+    number.style.color = "black";
+  }
+}
+function reset() {
+  number.textContent = 0;
+  number.style.color = "black";
+}
