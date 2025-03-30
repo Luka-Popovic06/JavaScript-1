9f4c023af2f0852f61c7373401eef1a9c0d449ac
Luka Popovic <lukapopovic0111@gmail.com>
2025-03-30 19:57:15 +0200

    Completed project

--- script.js
+++ script.js
@@ -16,25 +16,26 @@ const btnOne = document.querySelector("#btn-1");
 const btnTwo = document.querySelector("#btn-2");
 const btnThree = document.querySelector("#btn-3");
 const number = document.querySelector("p");
-
+let caunt = 0;
 function calculate(numberOne) {
   if (numberOne === btnOne) {
-    number.textContent = Number(number.textContent) - 1;
+    number.textContent = caunt -= 1;
   } else {
-    number.textContent = Number(number.textContent) + 1;
+    number.textContent = caunt += 1;
   }
 }
 function color() {
-  if (number.textContent < 0) {
+  if (caunt < 0) {
     number.style.color = "red";
-  } else if (number.textContent > 0) {
+  } else if (caunt > 0) {
     number.style.color = "green";
   } else {
     number.style.color = "black";
   }
 }
 function reset() {
-  number.textContent = 0;
+  caunt = 0;
+  number.textContent = caunt;
   number.style.color = "black";
 }
 btnOne.addEventListener("click", function () {
@@ -43,6 +44,8 @@ btnOne.addEventListener("click", function () {
 });
 btnTwo.addEventListener("click", reset);
 btnThree.addEventListener("click", function () {
+  //caunt = caunt + 1;
+  //number.textContent = caunt;
   calculate(btnThree);
   color();
 });
